97327fb31e97ae625947a8a01341e5a3d841313b
JoaoLeonardo <joaolhd18@gmail.com>
2021-07-05 22:08:18 -0300

    Commit 1

---
+++ aula2/src/main/java/br/edu/utfpr/pb/pwaula/PwaulaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.pwaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwaulaApplication.class, args);
+	}
+
+}
